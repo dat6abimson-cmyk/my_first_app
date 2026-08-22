a51a351d3bb48176c4702a77a983eb1f5537f9d0
Trong Dat <dat6abimson@gmail.com>
2026-08-22 21:33:56 +0700

    học Container

--- lib/main.dart
+++ lib/main.dart
@@ -10,19 +10,22 @@ void main() {
             style: TextStyle(
               fontSize: 40,
               fontWeight: FontWeight.w300,
-              color: Colors.blue,
+              color: Colors.red,
             ),
           ),
         ),
-        body: Text(
-          "Hello World",
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.w300,
-            color: Colors.red,
+        body: Container(
+          alignment: Alignment.center,
+          width: 250,
+          height: 100,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            border: Border.all(color: Colors.black, width: 5),
           ),
+          child: Text('Hello'),
         ),
       ),
+      debugShowCheckedModeBanner: false,
     ),
   );
 }
